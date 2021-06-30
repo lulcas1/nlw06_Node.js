cb1dd3ed649c1a9b406f753ab5f9367908285399
Lucas Araujo <araujo.v.lucas@gmail.com>
2021-06-30 16:58:52 -0300

    criação de usuário

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,15 @@
 import "reflect-metadata";
 import express from "express";
 
-import "./database"
+import { router } from "./routes";
+
+import "./database";
 
 const app = express();
+
+app.use(express.json());
+
+app.use(router);
 
 //http://localhost:3000 definindo porta do servidor
 app.listen(3000, () => console.log("Server is running "));
